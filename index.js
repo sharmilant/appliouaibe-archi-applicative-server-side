ab780c6678a274f1cd405eb60daa56d6c9d73815
Sharmilan <169923133+sharmilant@users.noreply.github.com>
2025-03-14 11:47:24 +0000

    2.4 - Micro-service de gestion de messages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express'); //import de la bibliothèque Express
 var app = express(); //instanciation d'une application Express
 
@@ -84,6 +85,68 @@ app.get('/cpt/inc', function(req, res) {
     res.json(responseSuccess);
     console.log("Incremented Counter by 1:", cpt.value);
   }
+});
+
+/*******************************
+ * 2.4 Un micro-service avec un état
+ *******************************/
+
+var allMsgs = ["Hello World", "foobar", "CentraleSupelec Forever"]
+
+
+// Route /msg/get/:msgId to get a specific message
+app.get("/msg/get/*", function(req, res) {
+
+  let id = req.params[0];
+  if (parseInt(id) || id == 0) {
+    id = parseInt(id);
+    if (id < allMsgs.length) {
+      msgToSend = { "code": 1, "msg": "" };
+      msgToSend.msg = allMsgs[id];
+      res.json(msgToSend);
+      console.log("Successfule get message with id:", id);
+    } else {
+      res.json({ "code": 0 });
+      console.log("Error get message with id, length:", id);
+    }
+  }
+  else {
+    res.json({ "code": 0 });
+    console.log("Error get message with id, provided parameters:", id);
+  }
+
+})
+
+//Route /msg/nber to get the number of messages
+app.get("/msg/nber", function(req, res) {
+  let response = { "code": 1, "msg": allMsgs.length };
+  res.json(response);
+  console.log("Successful get number of messages");
+}
+);
+
+//Route /msg/getAll to get all messages
+app.get("/msg/getAll", function(req, res) {
+  let response = { "code": 1, "msg": allMsgs };
+  res.json(response);
+});
+
+//Route /msg/post/:msg to post a new message
+app.get("/msg/post/*", function(req, res) {
+  let response = { "code": 0, "msg": 0 };
+  try {
+    let newMsg = req.params[0];
+    allMsgs.push(unescape(newMsg));
+    response.code = 1;
+    response.msg = allMsgs.length - 1;
+    res.json(response);
+    console.log("Successful post message:", newMsg);
+  }
+  catch (e) {
+    console.log("Error post message:", e);
+    res.json(response);
+  }
+
 });
 
 
