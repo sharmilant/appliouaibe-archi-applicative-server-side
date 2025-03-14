2dcee5ea89d2509034537077ec24a520220487ea
Sharmilan <169923133+sharmilant@users.noreply.github.com>
2025-03-14 11:15:52 +0000

    Added 2.3 - Un micro-service avec un état

--- index.js
+++ index.js
@@ -15,8 +15,81 @@ app.get("/", function(req, res) {
   res.send("Hello")
 })
 
+let count = 0;
+
+let counter =
+{
+  name: "counter",
+  count: 0,
+}
+
+app.get("/update_cmpt", function(req, res) {
+  counter.count++;
+  counter.count = parseInt(req.query["v"]);
+  res.json(counter);
+})
 
 
-app.listen(8080); //commence à accepter les requêtes
+/*******************************
+ * 2.1 Configuration
+ *******************************/
+
+let msg =
+{
+  msg: ""
+}
+
+
+app.get('/test/*', function(req, res) {
+  let responseObj = {
+    msg: req.params[0]
+  };
+  res.json(responseObj);
+});
+
+
+/*******************************
+ * 2.3 Un micro-service avec un état
+ *******************************/
+// Déclaration du compteur global
+let cpt = { value: 0 };
+
+// Route /cpt/query : retourne la valeur du compteur
+app.get('/cpt/query', function(req, res) {
+  res.json(cpt);
+  console.log("Returned Counter:", cpt.value);
+});
+
+// Route /cpt/inc : incrémente de 1 ou d'une valeur donnée
+app.get('/cpt/inc', function(req, res) {
+  let responseSuccess = { "code": 0 };
+  let responseError = { "code": -1 };
+
+  // Vérifier si un paramètre v est passé
+  if (req.query.v !== undefined) {
+    let increment = req.query.v;
+
+    // Vérifier si c'est un entier positif
+    if (/^\d+$/.test(increment)) {
+      cpt.value += parseInt(increment);
+      res.json(responseSuccess);
+      console.log(`Incremented Counter by ${increment}:`, cpt.value);
+    } else {
+      res.json(responseError);
+      console.log("Invalid increment value:", increment);
+    }
+  } else {
+    // Incrémentation simple de 1
+    cpt.value++;
+    res.json(responseSuccess);
+    console.log("Incremented Counter by 1:", cpt.value);
+  }
+});
+
+
+
+app.listen(8080, "0.0.0.0"); //commence à accepter les requêtes
 console.log("App listening on port 8080...");
+
+
 
